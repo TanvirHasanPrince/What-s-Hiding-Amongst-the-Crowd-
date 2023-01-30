96b3a02ebfbc9751a5961e9cedf8e5035ed5d83c
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2023-01-30 12:08:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+What's Hiding Amongst the Crowd? A word is on the loose and now has tried to hide amongst a crowd of tall letters! 
+
+Help write a function to detect what the word is, knowing the following rules:
+ • The wanted word is in lowercase. 
+ • The crowd of letters is all in uppercase. 
+ • Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+ 
+ Examples
+detectWord("UcUNFYGaFYFYGtNUH") ➞ "cat"
+detectWord("bEEFGBuFBRrHgUHlNFYaYr") ➞ "burglar"
+detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment") ➞ "embezzlement"
+*/
+
+
+const detectWord = (string) => {
+ let word = '';
+ for (let i = 0; i < string.length; i++) {
+ if (string[i] === string[i].toLowerCase()) {
+  word += string[i];
+
+ }
+
+   
+ }
+
+ return word;
+}
+
+console.log(detectWord("UcUNFYGaFYFYGtNUH"));
+console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
+console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
